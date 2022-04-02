92471d520b46e7ca0b9a3fb43ddec3b53c2fd3ed
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-02 22:36:40 +0530

    PROGRAM FOR STRCMP

--- strcmp.c
+++ strcmp.c
@@ -2,15 +2,16 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[100], str2[100];
-    printf("Enter String: ");
-    gets(str);
+    char str1[100], str2[100];
+    int i;
+    printf("Enter String 1: ");
+    gets(str1);
     printf("Enter String 2: ");
     gets(str2);
-    if(strcmp(str, str2) == 0)
-        printf("Two Strings are same");
+    i = strcmp(str1, str2);
+    if(i == 0)
+        printf("Two Strings are same\n");
     else
-        printf("Two Strings are different");
+        printf("Two Strings are different\n");
+    printf("Difference Between strings is %d", i);
 }
-            
-            
