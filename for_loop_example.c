4e3020af9c442d46c6c749a2e03d18cbf67dd628
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-02 13:03:44 +0530

    FOR LOOP EXAMPLE

---
+++ for_loop_example.c
@@ -0,0 +1,10 @@
+/* Program to print 1 to 10 numbers using for loop by Patan Musthakheem Khan */
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=0;i<=10;i++){
+        printf("%d\t", i);
+    }
+    getch();
+}
+  
