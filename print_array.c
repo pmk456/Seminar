a0fd5104b4bf510f784b11dfaaa65bf012882632
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-02 20:43:05 +0530

    PRINT ARRAY ELEMENTS FROM USER AND PRINT IT

---
+++ print_array.c
@@ -0,0 +1,12 @@
+/* Program to print array elements */
+#include<stdio.h>
+int main(){
+  int a[10], n, i;
+  printf("Enter Array Size upto 10: ");
+  scanf("%d", &n);
+  for(i=0;i<n;i++)
+    scanf("%d", &a[i]);
+  for(i=0;i<n;i++)
+    printf("%d\t", a[i]);
+  getch();
+}
