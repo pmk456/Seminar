e1f2bb15bb150755ae1b0b9dc588f1b074adf853
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-02 12:51:19 +0530

    Create matrix_addition_simplified.c

---
+++ matrix_addition_simplified.c
@@ -0,0 +1,36 @@
+/* Program to Add Two given matrices by Patan Musthakheem Khan */
+/* Program to Add Two given matrices by Patan Musthakheem Khan */
+#include <stdio.h>
+int main(){
+    int i, j, row, col;
+    int a[10][10], b[10][10], c[10][10];
+    printf("Enter Total Rows: "); 
+    scanf("%d", &row);
+    printf("Enter Total Colums: "); 
+    scanf("%d", &col);
+    printf("Enter Matrix 1: \n");
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
+            scanf("%d", &a[i][j]);
+    }
+    printf("Enter Matrix 2: \n");
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
+            scanf("%d", &b[i][j]);
+    }
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
+            c[i][j] = a[i][j] + b[i][j];
+    }
+    printf("Addition of Matrix is: \n");
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
+            printf("%d\t", c[i][j]);
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
+
+
