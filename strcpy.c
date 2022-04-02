04f329f833b776d08adc354ca0c8e4b187a8241f
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-02 20:29:10 +0530

    PROGRAM FOR STRCPY()

---
+++ strcpy.c
@@ -0,0 +1,13 @@
+/* Program for strcpy() */
+#include<stdio.h>
+int main(){
+  char original[100];
+  char duplicate[100];
+  printf("Enter String: ");
+  // Reading string from user
+  gets(original);
+  strcpy(duplicate, original);
+  printf("Original String is %s\n" ,original);
+  printf("Duplicate String is %s\n", duplicate);
+  getch();
+}
